e8387dc565fac3b6e534523bb8757ed8b1d64248
William Hass <wm.hass@gmail.com>
2015-12-03 00:22:58 -0200

    Created forecast viewcontroller

---
+++ WeatherApp/Modules/Forecast/ForecastViewController.h
@@ -0,0 +1,13 @@
+//
+//  ForecastViewController.h
+//  WeatherApp
+//
+//  Created by William Hass on 12/3/15.
+//  Copyright © 2015 William Hass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ForecastViewController : UIViewController
+
+@end
---
+++ WeatherApp/Modules/Forecast/ForecastViewController.m
@@ -0,0 +1,18 @@
+//
+//  ForecastViewController.m
+//  WeatherApp
+//
+//  Created by William Hass on 12/3/15.
+//  Copyright © 2015 William Hass. All rights reserved.
+//
+
+#import "ForecastViewController.h"
+
+@interface ForecastViewController ()
+
+@end
+
+@implementation ForecastViewController
+
+
+@end
